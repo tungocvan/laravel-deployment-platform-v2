35ecfae15566cab17d817f8be65b4afd46c4ac0c
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-09 10:00:44 +0700

    feat: load package upgrade audit adapter

--- modules/package/commands/upgrade.sh
+++ modules/package/commands/upgrade.sh
@@ -3,5 +3,6 @@ set -Eeuo pipefail
 source "${PLATFORM_HOME:-/opt/laravel-deployment-platform-v2}/core/bootstrap.sh"
 source "$PLATFORM_HOME/core/lib/package.sh"
 source "$PLATFORM_HOME/modules/package/lib/package.sh"
+source "$PLATFORM_HOME/modules/package/lib/upgrade-audit.sh"
 
 package_upgrade "$@"
