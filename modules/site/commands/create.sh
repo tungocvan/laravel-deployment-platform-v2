56f170b15ca7363346ecb075eedd49d80242ab86
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-09 21:03:41 +0700

    feat: record create site ssl outcome

--- modules/site/commands/create.sh
+++ modules/site/commands/create.sh
@@ -10,4 +10,21 @@ source "$PLATFORM_HOME/modules/site/lib/provision.sh"
 source "$PLATFORM_HOME/modules/site/lib/site.sh"
 source "$PLATFORM_HOME/modules/site/lib/domain-preflight.sh"
 source "$PLATFORM_HOME/modules/site/lib/create-strategy.sh"
+source "$PLATFORM_HOME/modules/site/lib/create-ssl-policy.sh"
+
+create_name=""
+create_domain=""
+create_ssl=1
+for arg in "$@"; do
+  case "$arg" in
+    --name=*) create_name="${arg#*=}" ;;
+    --domain=*) create_domain="${arg#*=}" ;;
+    --no-ssl) create_ssl=0 ;;
+  esac
+done
+
 site_create "$@"
+
+if [[ -n "$create_name" && -n "$create_domain" ]]; then
+  site_create_record_ssl_status "$create_name" "$create_domain" "$create_ssl"
+fi
