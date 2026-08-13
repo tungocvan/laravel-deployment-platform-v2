c21f47d35d5d5926dec3e5ac18818646902152e0
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-13 12:58:03 +0700

    debug: add create pre-seed diagnostics

--- modules/site/lib/create-seed-policy.sh
+++ modules/site/lib/create-seed-policy.sh
@@ -4,14 +4,53 @@
 # Production create MUST NOT run DatabaseSeeder / `php artisan db:seed` globally.
 # It reuses the same production-safe allow-list as `platform site production-seed`.
 
+site_create_preseed_repository_diagnostics() {
+  local project_path="$1" compose_file="$2"
+  echo "[CREATE DEBUG] Pre-seed Laravel/autoload diagnostics"
+
+  site_create_repository_compose "$project_path" "$compose_file" \
+    exec -T app php artisan about >/dev/null
+
+  site_create_repository_compose "$project_path" "$compose_file" \
+    exec -T app php -r '
+require "/var/www/html/vendor/autoload.php";
+$c="Modules\\Role\\database\\seeders\\RolesAndPermissionsSeeder";
+echo "PRESEED class_exists=".(class_exists($c)?"true":"false").PHP_EOL;
+if (class_exists($c)) {
+    $r=new ReflectionClass($c);
+    echo "PRESEED class=".$r->getName().PHP_EOL;
+    echo "PRESEED file=".$r->getFileName().PHP_EOL;
+}
+'
+}
+
+site_create_preseed_platform_diagnostics() {
+  local project_path="$1"
+  echo "[CREATE DEBUG] Pre-seed Laravel/autoload diagnostics"
+
+  deploy_compose "$project_path" exec -T app php artisan about >/dev/null
+  deploy_compose "$project_path" exec -T app php -r '
+require "/var/www/html/vendor/autoload.php";
+$c="Modules\\Role\\database\\seeders\\RolesAndPermissionsSeeder";
+echo "PRESEED class_exists=".(class_exists($c)?"true":"false").PHP_EOL;
+if (class_exists($c)) {
+    $r=new ReflectionClass($c);
+    echo "PRESEED class=".$r->getName().PHP_EOL;
+    echo "PRESEED file=".$r->getFileName().PHP_EOL;
+}
+'
+}
+
 site_create_seed_repository() {
   local project_path="$1" compose_file="$2"
+  site_create_preseed_repository_diagnostics "$project_path" "$compose_file"
   echo "[CREATE SEED] Production allow-list"
   site_production_seed_repository_path "$project_path" "$compose_file"
 }
 
 site_create_seed_platform() {
   local project_path="$1"
+  site_create_preseed_platform_diagnostics "$project_path"
   echo "[CREATE SEED] Production allow-list"
   site_production_seed_platform_path "$project_path"
 }
