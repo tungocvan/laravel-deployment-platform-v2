7ea3e9c914aeedee12fffcb49ad7757d6f19043b
tungocvan <tungocvan@gmail.com>
2026-08-21 09:39:20 +0700

    feat(site): default CLI create to canonical Laravel repository

--- modules/site/commands/create.sh
+++ modules/site/commands/create.sh
@@ -8,6 +8,19 @@ source "$PLATFORM_HOME/modules/deploy/lib/readiness.sh"
 source "$PLATFORM_HOME/modules/nginx/lib/nginx.sh"
 source "$PLATFORM_HOME/modules/ssl/lib/ssl.sh"
 source "$PLATFORM_HOME/modules/site/lib/site.sh"
+source "$PLATFORM_HOME/modules/site/lib/repository.sh"
 source "$PLATFORM_HOME/modules/site/lib/provision.sh"
 source "$PLATFORM_HOME/modules/site/lib/create.sh"
+
+has_repo=0
+for arg in "$@"; do
+  case "$arg" in
+    --repo=*) has_repo=1 ;;
+  esac
+done
+
+if [[ "$has_repo" -eq 0 ]]; then
+  set -- "$@" "--repo=$(site_default_repo)"
+fi
+
 site_create "$@"
