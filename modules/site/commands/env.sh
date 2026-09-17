61c3b9990df30527fa2af937f5e311f38da21901
tungocvan <tungocvan@gmail.com>
2026-09-17 14:07:11 +0700

    feat(site): route site-aware env management actions

--- modules/site/commands/env.sh
+++ modules/site/commands/env.sh
@@ -7,4 +7,12 @@ source "$PLATFORM_HOME/modules/site/lib/site.sh"
 source "$PLATFORM_HOME/modules/site/lib/provision.sh"
 source "$PLATFORM_HOME/modules/site/lib/runtime.sh"
 source "$PLATFORM_HOME/modules/site/lib/operations.sh"
-site_ops_env_apply "$@"
+source "$PLATFORM_HOME/modules/site/lib/env-management.sh"
+
+site="${1:-}"
+action="${2:-}"
+case "$action" in
+  --keys) site_ops_env_list_keys "$site" ;;
+  --edit) site_ops_env_edit "$site" ;;
+  *) site_ops_env_apply "$@" ;;
+esac
